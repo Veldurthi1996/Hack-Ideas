2b610402f412847fd9412fbbc1c4f9a1c411ae20
Veldurthi1996 <37625052+Veldurthi1996@users.noreply.github.com>
2021-10-06 20:15:34 +0530

    route updated with gaurd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { LoginComponent } from 'src/login/Login.component';
+import { NewHackathonComponent } from 'src/newHackathon/newHackathon.component';
+import { AuthenticationGaurd } from 'src/routeGaurds/Authentication.gaurd';
+
 const routes: Routes = [
   {
     path: '',
@@ -9,7 +12,18 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    component: HomeComponent
+    component: HomeComponent,
+    canActivate: [AuthenticationGaurd]
+  },
+  {
+    path: 'hackathon',
+    component: NewHackathonComponent,
+    canActivate: [AuthenticationGaurd]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
   }
 ];
 
