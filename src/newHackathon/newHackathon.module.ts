514ba938d3d40512bb40398bc24260f2e3919c35
Veldurthi1996 <37625052+Veldurthi1996@users.noreply.github.com>
2021-10-06 19:37:24 +0530

    newHackathon UI updates

--- src/newHackathon/newHackathon.module.ts
+++ src/newHackathon/newHackathon.module.ts
@@ -1,8 +1,13 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { NewHackathonComponent } from "./newHackathon.component";
 
 @NgModule({
-    imports:[],
+    imports:[
+        FormsModule,
+        CommonModule
+    ],
     declarations:[NewHackathonComponent],
     exports: [NewHackathonComponent]
 })
